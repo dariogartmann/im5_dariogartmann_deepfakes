aa3c8a1126b82af051d918a8be1b21d09335cdc4
dariogartmann <mail@dariogartmann.ch>
2022-12-15 17:38:47 +0100

    gulp watch task works finally <3

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var reload = browserSync.reload;
 var sass = require('gulp-sass')(require ('sass'));
 var concat = require('gulp-concat');
 
@@ -19,7 +18,7 @@ gulp.task('css', function () {
         }))
         .pipe(postcss([cssnano()]))
         .pipe(gulp.dest('assets/css/'))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(browserSync.stream());
 });
  
 gulp.task('js', function () {
@@ -27,19 +26,24 @@ gulp.task('js', function () {
         .pipe(concat('app.js'))
         .pipe(uglify())
         .pipe(gulp.dest('assets/js/'))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(browserSync.stream());
 });
 
-gulp.task('build', gulp.series('js','css'), function() {
+gulp.task('build', gulp.series('js','css'));
+
+function watch() {
+    gulp.series('build');
     browserSync.init({
         server: {
-            baseDir: '/'
+            baseDir: './'
         }
     });
 
-    gulp.watch("./source/scss/**/*.+(css|scss)", ["css"]);
-    gulp.watch("./source/js/**/*.js", ["js"]);
-    gulp.watch("./*.html", reload);
-});
+    gulp.watch("./source/scss/**/*.+(css|scss)", gulp.series('css'));
+    gulp.watch("./source/js/**/*.js", gulp.series('js'));
+    gulp.watch("./*.html").on('change', browserSync.reload);
+}
 
 gulp.task('default', gulp.series('build'));
+
+exports.watch = gulp.series(watch);
